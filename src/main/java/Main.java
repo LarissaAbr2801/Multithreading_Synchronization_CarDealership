b2a280820514321fa9b47664b28cadcfdcc4c21e
LarissaAbr2801 <abramova.la.28@gmail.com>
2022-05-20 19:24:32 +0300

    delete delays

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,13 @@
 public class Main {
 
     final static int BUYERS_QUANTITY = 3;
-    final static int WORK_WITH_CLIENT = 2500;
 
     public static void main(String[] args) throws InterruptedException {
         CarDealership dealership = new CarDealership();
         Seller seller = new Seller("Производитель Toyota");
 
         for (int i = 0; i < BUYERS_QUANTITY; i++) {
-            Thread.sleep(WORK_WITH_CLIENT);
             new Thread(null, dealership::sellCar, "" + (i + 1) + "").start();
-            Thread.sleep(WORK_WITH_CLIENT);
             new Thread(null, () -> seller.produceCar(dealership), seller.getName()).start();
 
         }
